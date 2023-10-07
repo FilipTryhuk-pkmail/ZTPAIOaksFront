246a7989aed46c5cb0cb8042a9276dcff29173e9
Filip Tryhuk <filip.tryhuk@student.pk.edu.pl>
2023-10-07 07:30:23 +0200

    fetch from database

--- src/screens/HistoryScreen.js
+++ src/screens/HistoryScreen.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import '../css/old_css/history.css'
-import {useLocation} from 'react-router-dom';
+import {json, useLocation} from 'react-router-dom';
 
 function HistoryScreen() {
   const location = useLocation();
@@ -9,29 +9,29 @@ function HistoryScreen() {
   const [rows, setRows] = useState("");
 
   const getData = (period) =>{
-    fetch("https://markow.pl/login_form.php", {
-            method: "GET",
-            body: JSON.stringify({login: location.state.login, period: period}),
-            // headers: {'Content-Type': 'application/json'}
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              setBooksData(JSON.stringify([{"name":"Lord of the Rings","status":"0", date:"11.05.2022"},{"name":"Dune","status":"1", date:"11.05.2022"}]));
-              console.log(JSON.parse(booksData));
+    fetch("http://localhost:8080/books", {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJzZWxmIiwiZXhwIjoxNjk2NjYxNzkwLCJpYXQiOjE2OTY2NTA5OTAsInNjb3BlIjoicmVhZCJ9.iC1itQygCBs43KEpQaqiuSSypS23TsTBkPZrg-3iWTVMTlSHvxLj9j4zLQ3PZWU3sUe43UKiVZ0Sw5dUKcKOxY1FWDgNRCgoEmOWTWMszKAZM2Au1Q4mEdVsVweyFwZ3IoN28GHKYcpPo1_aD9v5McH8hNLdJqOXMrSLLOeSV4PnNvJP84t0bJF9ffYTMi-wKH1UHuPFtsMK6380Q5i8XzXn_Ya7pNmm9P_E-zLcpSsbfxmqt2nRyWZ0NTD1EG7OL4YvSIUv5vkaV48xH9urEkHqj-4r0SHWrup1oAyDUqWuvO8Zvpv3PdmIcosJfwGdDyoY-cPz8xZqhUH_CvMrnA'
+      }
+    })
+        .then((response) => response.json())
+        .then((fetched) => {
+          return Object.values(Object.values(fetched)[0])[0];
+        })
+        .then((data) => {
+          console.log(JSON.parse(JSON.stringify(data)))
 
-
-              let row_data = "";
-              for(let i = 0 ; i < JSON.parse(booksData).length ; i++)  
-              {
-                row_data += "<tr><td>"+ JSON.parse(booksData)[i].name +"</td><td>"+ JSON.parse(booksData)[i].status +"</td><td>"+ JSON.parse(booksData)[i].date +"</td></tr>";
-              }
-              setRows(row_data);
-           
-            })
-            .catch((error) => console.log(error));
+          let row_data = "";
+          for(let j = 0; j < 2; j++) {
+            row_data += "<tr><td>"+ JSON.parse(JSON.stringify(data))[j].title +"</td><td>"+ JSON.parse(JSON.stringify(data))[j].author +"</td><td>"+ JSON.parse(JSON.stringify(data))[j].publishingDate +"</td></tr>"
+          }
+          setRows(row_data);
+        })
+        .catch((error) => console.log(error));
   }
 
-  
 
   return (
   <>
@@ -75,19 +75,19 @@ function HistoryScreen() {
           <section className="HboxRows">
             <h2>Show:</h2>
             <div className="timeline">
-              <a onClick={() => getData('lastDay')} className="timeline_buttons">
+              <a onClick={() => getData(24)} className="timeline_buttons">
                 last day
               </a>
-              <a href="/#" className="timeline_buttons">
+              <a onClick={() => getData(24*7)} className="timeline_buttons">
                 last week
               </a>
-              <a href="/#" className="timeline_buttons">
+              <a onClick={() => getData(24*31)} className="timeline_buttons">
                 last month
               </a>
-              <a href="/#" className="timeline_buttons">
+              <a onClick={() => getData(24*365)} className="timeline_buttons">
                 last year
               </a>
-              <a href="/#" className="timeline_buttons">
+              <a onClick={() => getData(-1)} className="timeline_buttons">
                 all time
               </a>
             </div>
@@ -95,13 +95,15 @@ function HistoryScreen() {
           <section className="HboxRows">
             <h2>HISTORY</h2>
             <table>
-  <tr>
-    <th>Book Name</th>
-    <th>Status</th>
-    <th>Date</th>
-  </tr>
-  <tbody dangerouslySetInnerHTML={{ __html: rows }}/>
-</table>
+              <tbody>
+              <tr>
+                <th>Book Name</th>
+                <th>Status</th>
+                <th>Date</th>
+              </tr>
+              </tbody>
+              <tbody dangerouslySetInnerHTML={{ __html: rows }}/>
+            </table>
           </section>
         </div>
       </main>
--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,58 +1,190 @@
 import '../css/old_css/headers.css'
 import '../css/old_css/register.css'
+import React, {useState, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import {Form, Button, Row, Col, Container, Alert} from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom';
+import { register } from '../actions/userActions'
+import Loader from '../components/Loader'
 
-function RegisterScreen() {
+function RegisterScreen({location, history}) {
+
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [surname, setSurname] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [city, setCity] = useState('')
+  const [streetAddress, setStreetAddress] = useState('')
+  const [postalCode, setPostalCode] = useState('')
+  const [message, setMessage] = useState('')
+
+  const redirect = location ? location.search.split('=')[1] : '/'
+  const dispatch = useDispatch()
+  const reload = useNavigate()
+  const userRegister = useSelector(state => state.userRegister)
+  const { error, loading, userInfo } = userRegister
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match')
+    } else {
+      dispatch(register(name, email, password,surname,phoneNumber,city,streetAddress,postalCode))
+    }
+    reload('/')
+  }
 
   return (
-  <>
-    <title>REGISTER</title>
-    <section className="logos">
-      <div className="logo">
-        <img src="../img/logo.svg" alt="Golden Oaks"/>
+      <div>
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col xs={12} md={6}>
+              <h1 className='text-center py-4 '>Create account</h1>
+              {message && <Alert variant='danger'>{message}</Alert>}
+              {error && <Alert variant='danger'>{error}</Alert>}
+              {loading && <Loader />}
+              <Form onSubmit={submitHandler}>
+
+                <Form.Group className='py-3' controlId='name'>
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                      required
+                      type='name'
+                      placeholder='Enter name'
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                  >
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group className='py-3' controlId='email'>
+                  <Form.Label>Email Address</Form.Label>
+                  <Form.Control
+                      required
+                      type='email'
+                      placeholder='Enter Email'
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                  >
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group className='py-3' controlId='surname'>
+                  <Form.Label>Surname</Form.Label>
+                  <Form.Control
+                      required
+                      type='surname'
+                      placeholder='Enter surname'
+                      value={surname}
+                      onChange={(e) => setSurname(e.target.value)}
+                  >
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group className='py-3' controlId='phoneNumber'>
+                  <Form.Label>Phone number</Form.Label>
+                  <Form.Control
+                      required
+                      type='number'
+                      min={0}
+                      placeholder='Enter phone number'
+                      value={phoneNumber}
+                      onChange={(e) => setPhoneNumber(e.target.value)}
+                  >
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group className='py-3' controlId='city'>
+                  <Form.Label>City</Form.Label>
+                  <Form.Control
+                      required
+                      type='name'
+                      placeholder='Enter city'
+                      value={city}
+                      onChange={(e) => setCity(e.target.value)}
+                  >
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group className='py-3' controlId='streetAddress'>
+                  <Form.Label>Street address</Form.Label>
+                  <Form.Control
+                      required
+                      type='name'
+                      placeholder='Enter street address'
+                      value={streetAddress}
+                      onChange={(e) => setStreetAddress(e.target.value)}
+                  >
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group className='py-3' controlId='postalCode'>
+                  <Form.Label>Postal code</Form.Label>
+                  <Form.Control
+                      required
+                      type='name'
+                      placeholder='Enter postal code'
+                      value={postalCode}
+                      onChange={(e) => setPostalCode(e.target.value)}
+                  >
+                  </Form.Control>
+                </Form.Group>
+
+
+                <Form.Group className='py-3' controlId='password'>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                      required
+                      type='password'
+                      placeholder='Enter Password'
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                  >
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group className='py-3' controlId='passwordConfirm'>
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Control
+                      required
+                      type='password'
+                      placeholder='Confirm Password'
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                  >
+                  </Form.Control>
+                </Form.Group>
+
+                <Button type='submit' variant='primary'>
+                  Register
+                </Button>
+
+              </Form>
+
+              <Row className='py-5'>
+                <Col>
+                  Have an Account? <Link
+                    to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+                  Sign In
+                </Link>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
       </div>
-      <div className="logo_subtext">
-        <img
-            src="../img/logo_2.svg"
-            alt="Decentralized Book Exchange"
-        />
-      </div>
-    </section>
-    <section className="registration_container">
-      <h1>Register</h1>
-      <form className="register" action="register" method="POST">
-        <div className="messages">
-          {/*?php
-          if(isset($messages)){
-              foreach($messages as $message) {
-                  echo $message;
-              }
-          }
-          ?*/}
-        </div>
-        <input
-            id="input_fields"
-            name="email"
-            type="text"
-            placeholder="email@email.com"
-        />
-        <input
-            id="input_fields"
-            name="password"
-            type="password"
-            placeholder="password"
-        />
-        <input id="input_fields" name="name" type="text" placeholder="name"/>
-        <input
-            id="input_fields"
-            name="surname"
-            type="text"
-            placeholder="surname"
-        />
-        <button type="submit">REGISTER</button>
-      </form>
-    </section>
-  </>
   )
 }
 
-export default RegisterScreen;
+export default RegisterScreen
